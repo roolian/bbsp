f6b6f17a293fa0d261e7095bdf281448f55e2151
Julien sartory <jsartory@gmail.com>
2019-09-23 18:15:09 +0200

    fix css

--- web/app/themes/bbsp/app/setup.php
+++ web/app/themes/bbsp/app/setup.php
@@ -20,6 +20,19 @@ add_action('wp_enqueue_scripts', function () {
     }
 }, 100);
 
+//Remove JQuery migrate
+add_action('wp_default_scripts', function ($scripts) {
+        if (!is_admin() && isset($scripts->registered['jquery'])) {
+            $script = $scripts->registered['jquery'];
+
+            if ($script->deps) { // Check whether the script has any dependencies
+                $script->deps = array_diff($script->deps, array('jquery-migrate'));
+            }
+        }
+    }
+
+);
+
 /**
  * Theme setup
  */
