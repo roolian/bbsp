e3788e2e42036ef2de0d8cc89f857e5af08411d4
Julien sartory <jsartory@gmail.com>
2021-05-12 10:58:57 +0200

    fix

--- web/app/plugins/elementor-daeris-addons/elementor-daeris-addons.php
+++ web/app/plugins/elementor-daeris-addons/elementor-daeris-addons.php
@@ -9,7 +9,9 @@
  * Text Domain: elementor-daeris-addons
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
 
 /**
  * Main Elementor Hello World Class
@@ -22,167 +24,223 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Any custom code should go inside Plugin Class in the plugin.php file.
  * @since 1.2.0
  */
-final class Elementor_Daeris_Addons {
+final class Elementor_Daeris_Addons
+{
+    /**
+     * Plugin Version
+     *
+     * @since 1.0.0
+     * @var string The plugin version.
+     */
+    const VERSION = '1.0.0';
 
-	/**
-	 * Plugin Version
-	 *
-	 * @since 1.0.0
-	 * @var string The plugin version.
-	 */
-	const VERSION = '1.0.0';
+    /**
+     * Minimum Elementor Version
+     *
+     * @since 1.2.0
+     * @var string Minimum Elementor version required to run the plugin.
+     */
+    const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
 
-	/**
-	 * Minimum Elementor Version
-	 *
-	 * @since 1.2.0
-	 * @var string Minimum Elementor version required to run the plugin.
-	 */
-	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+    /**
+     * Minimum PHP Version
+     *
+     * @since 1.2.0
+     * @var string Minimum PHP version required to run the plugin.
+     */
+    const MINIMUM_PHP_VERSION = '7.0';
 
-	/**
-	 * Minimum PHP Version
-	 *
-	 * @since 1.2.0
-	 * @var string Minimum PHP version required to run the plugin.
-	 */
-	const MINIMUM_PHP_VERSION = '7.0';
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     *
+     * @access private
+     * @static
+     *
+     * @var Elementor_Test_Extension The single instance of the class.
+     */
+    private static $_instance = null;
 
-	/**
-	 * Constructor
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function __construct() {
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     * @static
+     *
+     * @return Elementor_Test_Extension An instance of the class.
+     */
+    public static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
 
-		// Load translation
-		add_action( 'init', array( $this, 'i18n' ) );
+        return self::$_instance;
+    }
 
-		// Init Plugin
-		add_action( 'plugins_loaded', array( $this, 'init' ) );
-	}
+    /**
+     * Constructor
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function __construct()
+    {
+        // Init Plugin
+        add_action('plugins_loaded', [$this, 'init']);
+    }
 
-	/**
-	 * Load Textdomain
-	 *
-	 * Load plugin localization files.
-	 * Fired by `init` action hook.
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 */
-	public function i18n() {
-		load_plugin_textdomain( 'elementor-daeris-addons' );
-	}
+    /**
+     * Load Textdomain
+     *
+     * Load plugin localization files.
+     * Fired by `init` action hook.
+     *
+     * @since 1.2.0
+     * @access public
+     */
+    public function i18n()
+    {
+        load_plugin_textdomain('elementor-daeris-addons');
+    }
 
-	/**
-	 * Initialize the plugin
-	 *
-	 * Validates that Elementor is already loaded.
-	 * Checks for basic plugin requirements, if one check fail don't continue,
-	 * if all check have passed include the plugin class.
-	 *
-	 * Fired by `plugins_loaded` action hook.
-	 *
-	 * @since 1.2.0
-	 * @access public
-	 */
-	public function init() {
+    /**
+     * Initialize the plugin
+     *
+     * Validates that Elementor is already loaded.
+     * Checks for basic plugin requirements, if one check fail don't continue,
+     * if all check have passed include the plugin class.
+     *
+     * Fired by `plugins_loaded` action hook.
+     *
+     * @since 1.2.0
+     * @access public
+     */
+    public function init()
+    {
+        if ($this->is_compatible()) {
+            // Load translation
+            add_action('init', [$this, 'i18n']);
+            // Once we get here, We have passed all validation checks so we can safely include our plugin
+            require_once 'plugin.php';
+        }
+    }
 
-		// Check if Elementor installed and activated
-		if ( ! did_action( 'elementor/loaded' ) ) {
-			add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
-			return;
-		}
+    /**
+     * Compatibility Checks
+     *
+     * Checks if the installed version of Elementor meets the plugin's minimum requirement.
+     * Checks if the installed PHP version meets the plugin's minimum requirement.
+     *
+     * @since 1.0.0
+     *
+     * @access public
+     */
+    public function is_compatible()
+    {
+        // Check if Elementor installed and activated
+        if (!did_action('elementor/loaded')) {
+            add_action('admin_notices', [$this, 'admin_notice_missing_main_plugin']);
 
-		// Check for required Elementor version
-		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
-			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
-			return;
-		}
+            return false;
+        }
 
-		// Check for required PHP version
-		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
-			add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
-			return;
-		}
+        // Check for required Elementor version
+        if (!version_compare(ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=')) {
+            add_action('admin_notices', [$this, 'admin_notice_minimum_elementor_version']);
 
-		// Once we get here, We have passed all validation checks so we can safely include our plugin
-		require_once( 'plugin.php' );
-	}
+            return false;
+        }
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have Elementor installed or activated.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_missing_main_plugin() {
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
+        // Check for required PHP version
+        if (version_compare(PHP_VERSION, self::MINIMUM_PHP_VERSION, '<')) {
+            add_action('admin_notices', [$this, 'admin_notice_minimum_php_version']);
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: Elementor */
-			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-daeris-addons' ),
-			'<strong>' . esc_html__( 'Elementor Daeris Addons', 'elementor-daeris-addons' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-daeris-addons' ) . '</strong>'
-		);
+            return false;
+        }
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-	}
+        return true;
+    }
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have a minimum required Elementor version.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_minimum_elementor_version() {
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have Elementor installed or activated.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_missing_main_plugin()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-daeris-addons' ),
-			'<strong>' . esc_html__( 'Elementor Daeris Addons', 'elementor-daeris-addons' ) . '</strong>',
-			'<strong>' . esc_html__( 'Elementor', 'elementor-daeris-addons' ) . '</strong>',
-			self::MINIMUM_ELEMENTOR_VERSION
-		);
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: Elementor */
+            esc_html__('"%1$s" requires "%2$s" to be installed and activated.', 'elementor-daeris-addons'),
+            '<strong>' . esc_html__('Elementor Daeris Addons', 'elementor-daeris-addons') . '</strong>',
+            '<strong>' . esc_html__('Elementor', 'elementor-daeris-addons') . '</strong>'
+        );
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-	}
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
 
-	/**
-	 * Admin notice
-	 *
-	 * Warning when the site doesn't have a minimum required PHP version.
-	 *
-	 * @since 1.0.0
-	 * @access public
-	 */
-	public function admin_notice_minimum_php_version() {
-		if ( isset( $_GET['activate'] ) ) {
-			unset( $_GET['activate'] );
-		}
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required Elementor version.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_minimum_elementor_version()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
 
-		$message = sprintf(
-			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
-			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-daeris-addons' ),
-			'<strong>' . esc_html__( 'Elementor Daeris Addons', 'elementor-daeris-addons' ) . '</strong>',
-			'<strong>' . esc_html__( 'PHP', 'elementor-daeris-addons' ) . '</strong>',
-			self::MINIMUM_PHP_VERSION
-		);
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-daeris-addons'),
+            '<strong>' . esc_html__('Elementor Daeris Addons', 'elementor-daeris-addons') . '</strong>',
+            '<strong>' . esc_html__('Elementor', 'elementor-daeris-addons') . '</strong>',
+            self::MINIMUM_ELEMENTOR_VERSION
+        );
 
-		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
-	}
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
+
+    /**
+     * Admin notice
+     *
+     * Warning when the site doesn't have a minimum required PHP version.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function admin_notice_minimum_php_version()
+    {
+        if (isset($_GET['activate'])) {
+            unset($_GET['activate']);
+        }
+
+        $message = sprintf(
+            /* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-daeris-addons'),
+            '<strong>' . esc_html__('Elementor Daeris Addons', 'elementor-daeris-addons') . '</strong>',
+            '<strong>' . esc_html__('PHP', 'elementor-daeris-addons') . '</strong>',
+            self::MINIMUM_PHP_VERSION
+        );
+
+        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+    }
 }
 
 // Instantiate Elementor_Daeris_Addons.
-new Elementor_Daeris_Addons();
+Elementor_Daeris_Addons::instance();
