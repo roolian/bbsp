b57f43bb3343db6c9f4f026fd1fdd06fad3031e0
Julien sartory <jsartory@gmail.com>
2022-04-11 18:23:44 +0200

    fix yoast 2

--- web/app/themes/daerisone/includes/yoast.php
+++ web/app/themes/daerisone/includes/yoast.php
@@ -4,7 +4,8 @@ add_filter( 'wpseo_robots', 'yoast_seo_robots' );
 
 function yoast_seo_robots( $robots ) {
 
-    if ( is_front_page() && get_locale() == 'fr-FR' ) {
+
+    if ( is_front_page() && get_locale() == 'fr_FR' ) {
         $robots = 'noindex, follow';
     }
 
